d31b030c659dc0c06d4ba7913432617ab3ae7540
shariarf39 <shariarf39@gmail.com>
2023-11-25 20:27:10 +0600

    music

---
+++ app/src/main/java/com/diydiscovery/rhythmbit/MainActivity.java
@@ -0,0 +1,30 @@
+package com.diydiscovery.rhythmbit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.btn_start);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2(){
+        Intent intent=new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
